ef489abb7c5dd034c5a8c175204b4ac552c005cd
Sjors Wijsman <sjorswijsman@gmail.com>
2021-03-09 11:38:50 +0100

    Express & ejs install

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("test")
+const express = require('express');
+const app = express();
+
+// Set view engine to ejs
+app.set('view engine', 'ejs');
+
+// Home page
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+// Open app on port
+const port = process.env.PORT || 3000;
+app.listen(port);
+console.log(`App listening on ${port}`);
